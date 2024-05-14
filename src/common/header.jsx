e6ca23fe7776b45178e993a2c3b726decb625e87
adurbek <145314002+adurbek@users.noreply.github.com>
2024-05-14 17:45:24 +0500

    gkuhk

--- src/common/header.jsx
+++ src/common/header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
   const [navbarIsActive, setnavbarIsActive] = useState(false);
 
   return (
-    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+    <nav className="bg-gray-900 dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <img src={logo} className="h-8" alt="Flowbite Logo" />
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
